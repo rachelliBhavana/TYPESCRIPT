c35c58cce4056cf1ae0ff565c298063bebfc36ab
rachelliBhavana <86688714+rachelliBhavana@users.noreply.github.com>
2021-07-28 23:04:23 +0530

    Add files via upload

---
+++ function.ts
@@ -0,0 +1,11 @@
+function addition(a:number,b:number):number
+{
+    return a+b;
+}
+var no1 : number = 10;
+var no2 : number =20;
+var ans : number = 0;
+
+ans =addition(no1,no2);
+
+console.log("addition is :",ans);
