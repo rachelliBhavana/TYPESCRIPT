6e5653eadd1bd2e0fb2ae052156e51749ee8dad4
rachelliBhavana <86688714+rachelliBhavana@users.noreply.github.com>
2021-08-01 21:54:45 +0530

    Add files via upload

---
+++ oop.ts
@@ -0,0 +1,19 @@
+//class declaration
+class arithematic
+{
+    //characteristics
+    no1:number;
+    no2:number;
+    ans:number;
+    //Behaviour
+    addition():number
+    {
+        this.ans = this.no1 + this.no2;
+        return this.ans;
+    }
+    substraction():number
+    {
+        this.ans = this.no1 - this.no2;
+        return this.ans;
+    }
+}
