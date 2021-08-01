57b94f2960c22d4e2da0f0c37080ed41a6734932
rachelliBhavana <86688714+rachelliBhavana@users.noreply.github.com>
2021-08-01 21:53:41 +0530

    Add files via upload

---
+++ switch.ts
@@ -0,0 +1,26 @@
+var Div:string="B";
+console.log("Marvellous : Switch case");
+
+switch(Div)
+{
+    case "A":
+        {
+            console.log("Time: 7AM to 11 AM");
+            break;
+        }
+    case "B":
+        {
+                console.log("9 AM TO 12 PM");
+                break;
+        }
+    case "C":
+        {
+                console.log("Time : 1 PM TO 6 PM ");
+                break;
+        }
+    default:
+       {
+                console.log("Invalid Division");
+                break;
+        } 
+}
