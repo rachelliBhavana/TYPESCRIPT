18201513a341943c2b1dd91f04c7c5947fa4b762
rachelliBhavana <86688714+rachelliBhavana@users.noreply.github.com>
2021-08-01 21:54:00 +0530

    Add files via upload

---
+++ ifelse.ts
@@ -0,0 +1,15 @@
+console.log("Marvellous : if else construction");
+function chkevenodd(num:number)
+{
+    if(num%2==0)
+    {
+        console.log("even");
+    }
+    else
+    {
+        console.log("odd");
+    }
+}
+
+var no:number=12;
+chkevenodd(no);
